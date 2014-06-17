0fc8cf97eeace1ba1e91da686351b4fa9e3a89cf
Cesar <cesar6134@gmail.com>
2014-06-17 10:21:57 +0800

    Toggle Comment

--- bin/ex05_prepare.dart
+++ bin/ex05_prepare.dart
@@ -4,9 +4,9 @@ void main() {
   var a = 'I am smart. So I did it.',
       b = 'I am smart So I did it';
   print('${onlyLetters(a)} ${onlyLetters(b)}');
-//  print(compareSentences(a, b));
-//  print(timesTable(8, 9));
-//  print(asteriskTree(5));
-//  print(longestWord(a));
+  print(compareSentences(a, b));
+  print(timesTable(8, 9));
+  print(asteriskTree(5));
+  print(longestWord(a));
 
 }
