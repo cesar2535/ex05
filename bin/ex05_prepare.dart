609ec1511d33abc2725fbda48da93f18cc616d0e
Cesar <cesar6134@gmail.com>
2014-06-17 09:54:05 +0800

    Complete exercise 05

---
+++ bin/ex05_prepare.dart
@@ -0,0 +1,12 @@
+import 'ex05_func.dart';
+
+void main() {
+  var a = 'I am smart. So I did it.',
+      b = 'I am smart So I did it';
+  print('${onlyLetters(a)} ${onlyLetters(b)}');
+//  print(compareSentences(a, b));
+//  print(timesTable(8, 9));
+//  print(asteriskTree(5));
+//  print(longestWord(a));
+
+}
